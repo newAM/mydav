291d2d641b3a71e4234aa344f8737068cbe35896
Alex Martens <alex@thinglab.org>
2026-01-04 10:11:59 -0800

    flake.nix: remove crane

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 I was looking for a WebDAV server, but at the time of writing (2023-07-24) I
 found:
 
-* The popular go implementation archived 2022-09: <https://github.com/hacdias/webdav>
-* The rust implementation unmaintained since 2022-05: <https://github.com/miquels/webdav-server-rs>
-* The nginx extension unmaintained since 2018-12: <https://github.com/arut/nginx-dav-ext-module>
+- The popular go implementation archived 2022-09: <https://github.com/hacdias/webdav>
+- The rust implementation unmaintained since 2022-05: <https://github.com/miquels/webdav-server-rs>
+- The nginx extension unmaintained since 2018-12: <https://github.com/arut/nginx-dav-ext-module>
 
 I also tried [SeaweedFS](https://github.com/seaweedfs/seaweedfs),
 but I found that it was too complex to quickly debug issues I was running into.
@@ -23,22 +23,22 @@ a more mature solution.
 This is designed to be used with [NixOS], but should work with most Linux
 distributions.
 
-* Add this repository to your flake inputs:
+- Add this repository to your flake inputs:
 
 ```nix
 {
   inputs = {
-    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     mydav.url = "github:newam/mydav";
-    mydav.inputs.nixpkgs.follows = "unstable";
+    mydav.inputs.nixpkgs.follows = "nixpkgs";
+    dp800.inputs.treefmt.follows = "";
   };
 }
 ```
 
-* Add `mydav.overlays.default` to `nixpkgs.overlays`.
-* Import the `mydav.nixosModules.default` module.
-* Configure.
-
+- Add `mydav.overlays.default` to `nixpkgs.overlays`.
+- Import the `mydav.nixosModules.default` module.
+- Configure.
 
 [NixOS]: https://nixos.org/
--- flake.nix
+++ flake.nix
@@ -2,61 +2,130 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    crane.url = "github:ipetkov/crane";
-
     advisory-db.url = "github:rustsec/advisory-db";
     advisory-db.flake = false;
+
+    treefmt.url = "github:numtide/treefmt-nix";
+    treefmt.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
     self,
-    advisory-db,
     nixpkgs,
-    crane,
+    advisory-db,
+    treefmt,
   }: let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    craneLib = crane.mkLib pkgs;
+    forEachSystem = nixpkgs.lib.genAttrs [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ];
 
-    src = craneLib.cleanCargoSource self;
+    cargoToml = nixpkgs.lib.importTOML ./Cargo.toml;
 
-    cargoArtifacts = craneLib.buildDepsOnly {inherit src;};
+    # https://github.com/ipetkov/crane/blob/112e6591b2d6313b1bd05a80a754a8ee42432a7e/lib/cleanCargoSource.nix
+    cargoSrc = nixpkgs.lib.cleanSourceWith {
+      # Apply the default source cleaning from nixpkgs
+      src = nixpkgs.lib.cleanSource self;
+      # https://github.com/ipetkov/crane/blob/112e6591b2d6313b1bd05a80a754a8ee42432a7e/lib/filterCargoSources.nix
+      filter = orig_path: type: let
+        path = toString orig_path;
+        base = baseNameOf path;
+        parentDir = baseNameOf (dirOf path);
+
+        matchesSuffix = nixpkgs.lib.any (suffix: nixpkgs.lib.hasSuffix suffix base) [
+          # Keep rust sources
+          ".rs"
+          # Keep all toml files as they are commonly used to configure other
+          # cargo-based tools
+          ".toml"
+        ];
+
+        # Cargo.toml already captured above
+        isCargoFile = base == "Cargo.lock";
+
+        # .cargo/config.toml already captured above
+        isCargoConfig = parentDir == ".cargo" && base == "config";
+      in
+        type == "directory" || matchesSuffix || isCargoFile || isCargoConfig;
+    };
+
+    treefmtEval = pkgs:
+      treefmt.lib.evalModule pkgs {
+        projectRootFile = "flake.nix";
+        programs = {
+          alejandra.enable = true;
+          prettier.enable = true;
+          rustfmt.enable = true;
+          taplo.enable = true;
+        };
+      };
+
+    overlay = final: prev: {
+      mydav = prev.rustPlatform.buildRustPackage {
+        pname = cargoToml.package.name;
+        version = cargoToml.package.version;
+
+        src = cargoSrc;
+
+        cargoDeps = prev.rustPlatform.importCargoLock {
+          lockFile = ./Cargo.lock;
+        };
+
+        nativeCheckInputs = [
+          prev.cargo-audit
+          prev.clippy
+        ];
+
+        preCheck = ''
+          echo "Running cargo audit..."
+          cargo audit -n -d ${advisory-db} --ignore yanked
+
+          echo "Running clippy..."
+          cargo clippy -- -Dwarnings
+        '';
+
+        meta = {
+          description = cargoToml.package.description;
+          homepage = cargoToml.package.repository;
+          license = prev.lib.licenses.mit;
+          maintainers = [prev.lib.maintainers.newam];
+        };
+      };
+    };
   in {
-    packages.x86_64-linux.default =
-      craneLib.buildPackage
-      {
-        inherit cargoArtifacts src;
+    packages = forEachSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [overlay];
+        };
+      in {
+        default = pkgs.mydav;
+      }
+    );
+
+    formatter = forEachSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+        (treefmtEval pkgs).config.build.wrapper
+    );
+
+    checks = forEachSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [overlay];
       };
-
-    overlays.default = final: prev: {
-      mydav = self.packages.${prev.system}.default;
-    };
-    nixosModules.default = import ./module.nix;
-
-    checks.x86_64-linux = let
-      nixSrc = nixpkgs.lib.sources.sourceFilesBySuffices self [".nix"];
     in {
-      pkg = self.packages.x86_64-linux.default;
+      mydav = pkgs.mydav;
 
-      audit = craneLib.cargoAudit {
-        inherit src advisory-db;
-      };
+      formatting = (treefmtEval pkgs).config.build.check self;
+    });
 
-      clippy = craneLib.cargoClippy {
-        inherit cargoArtifacts src;
-        cargoClippyExtraArgs = "-- --deny warnings";
-      };
+    overlays.default = overlay;
 
-      rustfmt = craneLib.cargoFmt {inherit src;};
-
-      alejandra = pkgs.runCommand "alejandra" {} ''
-        ${pkgs.alejandra}/bin/alejandra --check ${nixSrc}
-        touch $out
-      '';
-
-      statix = pkgs.runCommand "statix" {} ''
-        ${pkgs.statix}/bin/statix check ${nixSrc}
-        touch $out
-      '';
-    };
+    nixosModules.default = import ./module.nix;
   };
 }
